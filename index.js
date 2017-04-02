61d58f944cf3e03c2399e1268bebc42f8f263326
Игорь Огниченко <ognichenko.igor@gmail.com>
2017-04-03 00:41:28 +0300

    cs fix

--- index.js
+++ index.js
@@ -5,34 +5,34 @@ var fileExists = require('file-exists');
 var path = require('path');
 
 module.exports = function (logFile, basenamePrefix) {
-    logFile = logFile || './log.json';
-    basenamePrefix = basenamePrefix || '';
-    var manifest = [];
+  logFile = logFile || './log.json';
+  basenamePrefix = basenamePrefix || '';
+  var manifest = [];
 
-    return through.obj(
+  return through.obj(
         function (file, enc, cb) {
-            if (!file.path) {
-                cb();
+          if (!file.path) {
+            cb();
 
-                return;
-            }
+            return;
+          }
 
-            var asset = basenamePrefix + file.basename;
+          var asset = basenamePrefix + file.basename;
 
-            if (manifest.indexOf(asset) === -1) {
-                manifest.push(asset);
-            }
+          if (manifest.indexOf(asset) === -1) {
+            manifest.push(asset);
+          }
 
-            cb(null, file);
+          cb(null, file);
         },
         function (cb) {
-            if (!fileExists(path.dirname(logFile))) {
-                mkpath.sync(path.dirname(logFile));
-            }
+          if (!fileExists(path.dirname(logFile))) {
+            mkpath.sync(path.dirname(logFile));
+          }
 
-            jsonfile.writeFile(logFile, manifest, {spaces: 2}, function (err) {
-                cb(err);
-            })
+          jsonfile.writeFile(logFile, manifest, {spaces: 2}, function (err) {
+            cb(err);
+          });
         }
     );
 };
